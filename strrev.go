cb5c3adba933cd81ea90df43602b618f114bc71a
TalgaPola <talgat86@inbox.ru>
2019-10-17 23:41:53 +0600

    strrev.go

---
+++ strrev.go
@@ -0,0 +1,19 @@
+package piscine
+
+func change(str string) int {
+	len_1 := 0
+	str1 := []rune(str)
+	for i := range str1 {
+		len_1 = i + 1
+	}
+	return len_1
+}
+
+func StrRev(s string) string {
+	last_string := ""
+	len_s := change(s)
+	for i := len_s - 1; i >= 0; i-- {
+		last_string += string(s[i])
+	}
+	return last_string
+}
